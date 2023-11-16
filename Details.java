4feab7226a71cd0bdf2de901b3e215b9e6d226a5
Kasika11 <151042192+Kasika11@users.noreply.github.com>
2023-11-16 22:46:50 +0300

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,12 @@
+public class Details{
+public static void main(String[]args){
+System.out.println("Institution:KCA University");
+System.out.println("Course:DIT");
+System.out.println("Name:Cecilia Kasika Ngikwa");
+System.out.println("Reg no:22/03649");
+System.out.println("Gender:Female");
+System.out.println("DOB:20.06.2003");
+System.out.println("Phone no:+254794364606");
+System.out.println("Email Address:kasikacecilia@gmail.com");
+}
+}
